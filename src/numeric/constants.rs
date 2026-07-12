788f55a1e8f089621023a8766ccb5820eefff592
Asgard <git@asgard.io>
2026-07-12 00:34:00 -0600

    chore: merge bounded into constants

--- src/numeric/constants.rs
+++ src/numeric/constants.rs
@@ -22,3 +22,12 @@ pub trait Half {
 pub trait NegOne {
     const NEG_ONE: Self;
 }
+
+/// A trait for types that are bounded, meaning they have a minimum and maximum value.
+pub trait Bounded {
+    /// The minimum value of the type.
+    const MIN: Self;
+
+    /// The maximum value of the type.
+    const MAX: Self;
+}
